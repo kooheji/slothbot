c56b454ad76563a117f95cb77025613fd6ed2e43
Ahmed Alkooheji <ahmed@kooheji.info>
2024-02-27 17:36:22 +0300

    Update 0.0.5 [New Dictionary m]

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require("express");
-const isWord = require("is-word");
+const checkWord = require("check-dictionary-word");
 const app = express();
 const PORT = 3000;
 
-app.get("/validate-word", (req, res) => {
-  const { word } = req.query;
+app.get("/validate-word/:word", (req, res) => {
+  const { word } = req.params;
 
-  if (word && word.length >= 5 && isWord(word)) {
-    const lastLetter = word[word.length - 1];
-    res.json({ result: lastLetter });
-  } else {
-    res
+  if (word.length < 5) {
+    return res
       .status(400)
-      .json({
-        error:
-          "Invalid word. Please provide a valid English word with at least 5 characters.",
-      });
+      .json({ error: "Word must be at least 5 letters long" });
   }
+
+  if (!checkWord(word)) {
+    return res.status(400).json({ error: "Not a valid English word" });
+  }
+
+  const lastLetter = word[word.length - 1];
+  res.json({ result: `${lastLetter}` });
 });
 
 app.listen(PORT, () => {
