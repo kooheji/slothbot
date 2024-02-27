463eea475a02090211f47a83cb2f4833c60a47ab
Ahmed Alkooheji <ahmed@kooheji.info>
2024-02-27 16:45:42 +0300

    Update 0.0.3

--- app.js
+++ app.js
@@ -1,60 +1,25 @@
 const express = require("express");
-const isWord = require("is-word");
-const axios = require("axios");
 const app = express();
-const PORT = 3000;
+const dictionary = require("dictionary-en");
 
-// Middleware to validate word length
-const validateWordLength = (req, res, next) => {
+app.get("/validate-word", (req, res) => {
   const { word } = req.query;
-  if (word.length < 5) {
-    return res
-      .status(400)
-      .json({ error: "Word must be at least 5 letters long" });
-  }
-  next();
-};
 
-// Middleware to validate word using Merriam-Webster Dictionary API
-const validateWord = async (req, res, next) => {
-  const { word } = req.query;
-  const apiKey = req.headers["x-dictionary-api-key"];
-  try {
-    const response = await axios.get(
-      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${apiKey}`
-    );
-    if (response.data.length === 0) {
-      return res.status(400).json({ error: "Invalid word" });
-    }
-    req.lastLetter = word[word.length - 1];
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
-// API endpoint to validate word and return last letter
-app.get("/validate", validateWordLength, validateWord, (req, res) => {
-  res.json({ lastLetter: req.lastLetter });
-});
-
-app.post("/validate-word", (req, res) => {
-  const { word } = req.body;
-
-  if (word.length >= 5 && isWord(word)) {
-    const lastLetter = word[word.length - 1];
-    res.json({ result: lastLetter });
+  if (word && word.length >= 5) {
+    dictionary(word, (valid) => {
+      if (valid) {
+        const lastLetter = word[word.length - 1];
+        res.json({ result: lastLetter });
+      } else {
+        res.status(400).json({ error: "Not a valid English word" });
+      }
+    });
   } else {
-    res
-      .status(400)
-      .json({
-        error:
-          "Invalid word. Please provide a real English word with at least 5 letters.",
-      });
+    res.status(400).json({ error: "Word must be at least 5 letters long" });
   }
 });
 
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
