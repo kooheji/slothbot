aff6e85b77d11bed760979c5c91b24eda71824bb
Ahmed Alkooheji <ahmed@kooheji.info>
2024-02-27 17:19:10 +0300

    Update 0.0.4

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require("express");
+const isWord = require("is-word");
 const app = express();
-const dictionary = require("dictionary-en");
+const PORT = 3000;
 
 app.get("/validate-word", (req, res) => {
   const { word } = req.query;
 
-  if (word && word.length >= 5) {
-    dictionary(word, (valid) => {
-      if (valid) {
-        const lastLetter = word[word.length - 1];
-        res.json({ result: lastLetter });
-      } else {
-        res.status(400).json({ error: "Not a valid English word" });
-      }
-    });
+  if (word && word.length >= 5 && isWord(word)) {
+    const lastLetter = word[word.length - 1];
+    res.json({ result: lastLetter });
   } else {
-    res.status(400).json({ error: "Word must be at least 5 letters long" });
+    res
+      .status(400)
+      .json({
+        error:
+          "Invalid word. Please provide a valid English word with at least 5 characters.",
+      });
   }
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
